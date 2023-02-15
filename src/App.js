a115bd302f21effc6a091b6f1d3a793bd631b4d3
Aditya Saxena <aaddii091@gmail.com>
2023-02-15 23:42:51 +0530

    RESTFull API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [num, setNum] = useState(0);
+  useEffect(() => {
+    document.title = `You Clicked Me ${num} Times!`;
+    async function getData() {
+      const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${num}/`);
+      console.log(res.data.name);
+      setNum(res.data.name.toUpperCase());
+    }
+    getData();
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>YOU HAVE CHOOSEN {num} !</h1>
+      <select
+        onChange={(e) => {
+          setNum(e.target.value);
+        }}
+      >
+        <option value="5">5</option>
+        <option value="25">25</option>
+        <option value="15">15</option>
+        <option value="2">2</option>
+        <option value="1">1</option>
+      </select>
+    </>
   );
 }
 
